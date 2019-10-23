4fe274a51334b6efa845154e34b7e0771c0ddac6
jieming.yu <jieming.yu@counect.com>
2019-10-23 16:27:30 +0800

    同步数据

--- src/main/java/com/counect/cube/ocrcomparejpa/controller/Rest.java
+++ src/main/java/com/counect/cube/ocrcomparejpa/controller/Rest.java
@@ -1,15 +1,22 @@
 package com.counect.cube.ocrcomparejpa.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.counect.cube.ocrcomparejpa.service.HDFSService;
+import com.counect.cube.ocrcomparejpa.utils.ContainerUtils;
+import com.counect.cube.ocrcomparejpa.utils.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @RestController
+@Slf4j
 public class Rest {
 
     @Autowired
@@ -18,6 +25,20 @@ public class Rest {
     @RequestMapping("hello")
     public String Hello(){
         return"hello";
+    }
+
+
+    @RequestMapping("syncMsid")
+    public String syncMsid(String body){
+        Set<String> usedMsids = new HashSet<>();
+        JSONArray jsonArray = JSONUtil.ArraytoJSONArray(body);
+        Set<Object> collect = jsonArray.stream().collect(Collectors.toSet());
+        for(Object object :collect){
+            usedMsids.add((String)object);
+        }
+        ContainerUtils.UsedMsids = usedMsids;
+        log.info(JSONUtil.toJson(collect));
+        return "123";
     }
 
     @RequestMapping("init")
---
+++ src/main/java/com/counect/cube/ocrcomparejpa/utils/ContainerUtils.java
@@ -0,0 +1,18 @@
+package com.counect.cube.ocrcomparejpa.utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class ContainerUtils {
+
+    public static Set<String > UsedMsids = new HashSet<>();
+}
